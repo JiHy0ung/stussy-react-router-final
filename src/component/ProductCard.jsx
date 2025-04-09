4e8a9dbbb8705fe20c3c8c02ec0d55a88d2823e8
JiHyoung You <fbwlgud51@gmail.com>
2025-04-09 21:32:30 +0900

    fix productcard issue

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -12,10 +12,13 @@ const ProductCard = ({item}) => {
   return (
     <div className='product-card' onClick={showDetail}>
         <img className='product-img' src={item?.img} alt="" />
-        <div className='choice'>{item?.choice == true? "Director choice" : "\u00A0"}</div>
-        <div className='product-name'>{item?.title}</div>
+        <div className="choice">
+            {item?.choice ? (
+                <span className="choice-true-text">Director choice</span>
+            ) : '\u00A0'}
+            </div>        <div className='product-name'>{item?.title}</div>
         <div>₩{item?.price}</div>
-        <div>{item?.new == true ? "New Arrivals" : "\u00A0"}</div>
+        <div className='product-new'>{item?.new == true ? "New Arrivals" : "\u00A0"}</div>
         <div className='product-sizes'>
             {item?.size?.join('\u00A0\u00A0\u00A0\u00A0')}
         </div>
